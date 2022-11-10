c80d4dd96e8d90a9fe2b9ff3b2cafcb61252b47c
Muzamil Khan <113015136+Muzamilofficial@users.noreply.github.com>
2022-11-10 17:01:50 +0500

    First commit

---
+++ IICT Project/slider.js
@@ -0,0 +1,13 @@
+function carousel() {var myIndex = 0;
+carousel();
+
+  var i;
+  var x = document.getElementsByClassName("slider");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 9000);    
+}
